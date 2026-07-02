a7d6faad6b92772c8e9eb0cecc49aea84d8bbf7b
salvo <dafder45678@gmail.com>
2026-07-02 10:23:10 +0200

    feat: beta 1.0

---
+++ app/quiz/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { useEffect, useState } from "react"
+import Image from "next/image"
+
+export default function StartQuiz() {
+    const [imgArray, setImgArray] = useState<string[]>([])
+
+    function refreshQuiz(){
+        window.location.reload()
+    }
+    useEffect(() => {
+        function createRandomQuiz() {
+            const imgArray: string[] = []
+            for (let i = 0; i < 10; i++) {
+                const randomIndex = Math.floor(Math.random() * (38 - 1) + 1)
+                console.log(`/${randomIndex}.png`)
+                imgArray.push(`/${randomIndex}.png`)
+            }
+            setImgArray(imgArray)
+        }
+
+        createRandomQuiz()
+    }, [])
+    return (
+        <div className="bg-base-200 min-h-screen p-4 md:p-8">
+            <div className="flex flex-col items-center text-center w-full">
+                
+                <div className="max-w-md mb-8">
+                    <h1 className="text-3xl font-bold">Quiz taragna (generato automaticamente)</h1>
+                    <p className="py-6">
+                        Il quiz è generato automaticamente partendo dai quiz presenti nelle cartelle tipologia sul sito
+                    </p>
+                </div>
+
+                <div className="w-full max-w-4xl flex flex-col items-center">
+                    {
+                        imgArray.map((img, index) => {
+                            return (
+                                <article key={index} className="mb-12 w-full">
+                                    <Image 
+                                        src={img} 
+                                        alt={`Quiz ${index + 1}`}   
+                                        width={1000} 
+                                        height={600} 
+                                        className="w-full h-auto rounded-xl shadow-lg bg-base-100" 
+                                    />
+                                </article>
+                            )
+                        })
+                    }
+                </div>
+                <button className="btn btn-primary" onClick={refreshQuiz}>Avvia nuovo quiz</button>
+                
+            </div>
+        </div>
+    )
+}
